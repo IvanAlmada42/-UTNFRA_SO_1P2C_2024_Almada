3c0b58d54eef8c34a703190f614441feeb5a0867
IvanAlmada42 <ivanalmada04@gmail.com>
2024-10-06 17:10:19 +0000

    ADD: subiendo punto A

--- RTA_SCRIPT_Examen_20241006/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -1,3 +1,9 @@
 #!BIM/BASH
 
+echo "Aca estoy creando la estructura de directorio"
 
+mkdir -p $HOME/Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+
+echo "Aca se va a mostrar la estructura"
+
+tree $HOME/Examenes-UTN
